9d43d8ac2dfdfd0411a08b175c68eefcc591a8b8
TechyGuy <65623992+TechyGuy17@users.noreply.github.com>
2020-10-19 07:33:13 +0200

    Added a slowdown on the darkmode

--- dark-light-mode.js
+++ dark-light-mode.js
@@ -1,6 +1,26 @@
-function darkmode()
-{
-    var element = document.body;
-  element.classList.toggle("dark-mode");
-
-}
+const buttonFunction = (function setup () { 
+  // const prevents from reassigning
+    const coolDown = 200 // 5s cooldown
+    let lastClick = Date.now() - coolDown // to start fresh
+  
+    function startCoolDown () {
+      lastClick = Date.now()
+    }
+    function checkCoolDown () {
+      const notOver = Date.now() - lastClick < coolDown
+      if (notOver) alert('stop spamming the button!')
+      // using an alert it will block javascript loops
+      return !notOver
+    }
+  
+    return function (darklightmode) {
+      if (checkCoolDown()) {
+        startCoolDown()
+  
+        var element = document.body;
+        element.classList.toggle("dark-mode");
+       
+      }
+    }
+  })() 
+  
