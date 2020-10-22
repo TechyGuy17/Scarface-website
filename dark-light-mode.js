06938fb4609b0434318355b909f87f15cafb9837
TechyGuy <65623992+TechyGuy17@users.noreply.github.com>
2020-10-22 12:11:45 +0200

    Now im 99% sure it should work

--- dark-light-mode.js
+++ dark-light-mode.js
@@ -1,31 +1,5 @@
-var isDarkMode = getCookie('darkmode');
-const buttonFunction = (function setup () { 
-  // const prevents from reassigning
-    const coolDown = 200 // 5s cooldown
-    let lastClick = Date.now() - coolDown // to start fresh
-  
-    function startCoolDown () {
-      lastClick = Date.now()
-    }
-    function checkCoolDown () {
-      const notOver = Date.now() - lastClick < coolDown
-      if (notOver) alert('stop spamming the button!')
-      // using an alert it will block javascript loops
-      return !notOver
-    }
-  
-    return function (darklightmode) {
-      if (checkCoolDown()) {
-        startCoolDown()
-  
-        var element = document.body;
-        element.classList.toggle("dark-mode");
-       
-      }
-    }
-  })() 
 
-   function setCookie(cname, cvalue, exdays) {
+  function setCookie(cname, cvalue, exdays) {
   var d = new Date();
   d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
   var expires = "expires="+d.toUTCString();
